0dddda7322cf3328cda59c556ce2fdfcc20ad175
Beanspoon <peter.s.kubiak@bath.edu>
2024-08-18 21:29:26 +0100

    Blinky example

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,27 @@
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use nrf52832_hal;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use nrf52832_hal as hal;
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let peripherals = hal::pac::Peripherals::take().unwrap();
+    let port0 = hal::gpio::p0::Parts::new(peripherals.P0);
+    let mut led0 = port0.p0_17.into_push_pull_output(hal::gpio::Level::High);
+    let mut led1 = port0.p0_18.into_push_pull_output(hal::gpio::Level::High);
+
+    let core_peripherals = hal::pac::CorePeripherals::take().unwrap();
+    let mut systick_delay = hal::Delay::new(core_peripherals.SYST);
+
+    loop {
+        led0.set_low().unwrap();
+        systick_delay.delay_ms(1000);
+        led1.set_low().unwrap();
+        systick_delay.delay_ms(1000);
+        led0.set_high().unwrap();
+        systick_delay.delay_ms(1000);
+        led1.set_high().unwrap();
+        systick_delay.delay_ms(1000);
+    }
 }

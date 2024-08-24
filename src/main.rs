0d58280395b85ea715a910b13ff47b1b55f1f4eb
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-24 01:36:22 +0100

    Fix issues running probe-rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 #![no_std]
 #![no_main]
 
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
+use nrf52832_hal as hal;
+use nrf52832_hal::gpio::Level;
+
 extern crate panic_halt;
 
-use cortex_m_rt::entry;
-use embedded_hal::{delay::DelayNs, digital::OutputPin};
-use nrf52832_hal as hal;
-
-#[entry]
+#[cortex_m_rt::entry]
 fn main() -> ! {
     let peripherals = hal::pac::Peripherals::take().unwrap();
     let port0 = hal::gpio::p0::Parts::new(peripherals.P0);
-    let mut led0 = port0.p0_17.into_push_pull_output(hal::gpio::Level::High);
-    let mut led1 = port0.p0_18.into_push_pull_output(hal::gpio::Level::High);
+    let mut led0 = port0.p0_17.into_push_pull_output(Level::High);
+    let mut led1 = port0.p0_18.into_push_pull_output(Level::High);
 
     let core_peripherals = hal::pac::CorePeripherals::take().unwrap();
     let mut systick_delay = hal::Delay::new(core_peripherals.SYST);
